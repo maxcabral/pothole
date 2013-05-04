006e5fe39b32fe8dabb2f9292ab9684dca359db4
Maxwell Cabral <max@maxcabral.com>
2013-05-04 16:40:11 -0700

    Add NSString -> URLEncoded string category

---
+++ Pothole Report/SupportingFiles/NSString+URLEncoding.h
@@ -0,0 +1,12 @@
+//
+//  NSString+URLEncoding.h
+//  Pothole Report
+//
+//  Created by Maxwell Cabral on 5/4/13.
+//  Copyright (c) 2013 Pothole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface NSString (URLEncoding)
+-(NSString *)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
+@end
