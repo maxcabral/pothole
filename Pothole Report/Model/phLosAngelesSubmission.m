c403c765e4b3d28599a187a14d252eb7d795aad4
Maxwell Cabral <max@maxcabral.com>
2013-05-04 14:02:52 -0700

    Flesh out the LA City specific code

--- Pothole Report/Model/phLosAngelesSubmission.m
+++ Pothole Report/Model/phLosAngelesSubmission.m
@@ -12,18 +12,43 @@
 
 -(void)submitWithName:(NSString*)name Address:(NSString*)address Phone:(NSString*)phone Email:(NSString*)email Description:(NSString*)desc Comment:(NSString*)comment AndLocation:(NSString*)loc
 {
+    [self submit:[[NSDictionary alloc] initWithObjects:[[NSArray alloc] initWithObjects:
+                                                        @"You+must+enter+a+Name",
+                                                        @"You+must+enter+a+Phone Number",
+                                                        @"Pothole+or+Request+for+Street+Repairs",
+                                                        name,
+                                                        address,
+                                                        phone,
+                                                        email,
+                                                        desc,
+                                                        loc,
+                                                        comment,
+                                                        nil]
+                                               forKeys:[[NSArray alloc] initWithObjects:
+                                                        @"name_required",
+                                                        @"phone_required"
+                                                        @"type",
+                                                        @"name",
+                                                        @"address",
+                                                        @"phone",
+                                                        @"email",
+                                                        @"desc",
+                                                        @"loc",
+                                                        @"comment",
+                                                        nil]]
+     ];
 /*
  @"http://nss/;acity.org/request.cfm"
- name_required = @"You+must+enter+a+Name"
- phone_required = @"You+must+enter+a+Phone Number"
- name =
- address =
- phone =
- email =
- type = @"Pothole+or+Request+for+Street+Repairs"
- desc
- loc
- comment
+ @"name_required = @"You+must+enter+a+Name"
+ @"phone_required = @"You+must+enter+a+Phone Number"
+ @"type", = @"Pothole+or+Request+for+Street+Repairs"
+ @"name",
+ @"address",
+ @"phone",
+ @"email",
+ @"desc",
+ @"loc",
+ @"comment",
  */
 }
 @end
