8f80a9fd23c6012cb9068f7282fe26f959206d5f
Will <will.ha82@gmail.com>
2013-05-04 14:12:49 -0700

    core location

--- Pothole Report/phViewController.m
+++ Pothole Report/phViewController.m
@@ -8,8 +8,17 @@
 
 #import "phViewController.h"
 
+@interface phViewController()
+
+- (void)updateLabels;
+
+@end
+
 @implementation phViewController {
     CLLocationManager *locationManager;
+    CLLocation *location;
+    BOOL updatingLocation;
+    NSError *lastLocationError;
 }
 
 @synthesize latitudeLabel;
@@ -19,9 +28,77 @@
 
 
 
+- (IBAction)getphLocation:(id)sender
+
+{
+    [self startLocationManager];
+    [self updateLabels];
+}
+
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)configureGetButton
+{
+    if (updatingLocation) {
+        [self.getButton setTitle:@"Stop" forState:UIControlStateNormal];
+    } else {
+        [self.getButton setTitle:@"Get My Location" forState:UIControlStateNormal];
+    }
+}
+
+- (void)updateLabels
+{
+    if (location != nil) {
+
+        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.latitude];
+        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", location.coordinate.longitude];
+        self.tagButton.hidden = NO;
+    } else {
+
+        self.latitudeLabel.text = @"";
+        self.longitudeLabel.text = @"";
+        self.addressLabel.text = @"";
+        self.tagButton.hidden = YES;
+        
+        NSString *statusMessage;
+        if (lastLocationError != nil) {
+            if ([lastLocationError.domain isEqualToString:kCLErrorDomain] && lastLocationError.code == kCLErrorDenied) {
+                statusMessage = @"Location Services Disabled";
+            } else {
+                statusMessage = @"Error Getting Location";
+            }
+        } else if (![CLLocationManager locationServicesEnabled]) {
+            statusMessage = @"Location Services Disabled";
+        } else if (updatingLocation) {
+            statusMessage = @"Searching...";
+        } else {
+            statusMessage = @"Press the Button to Start";
+        }
+        
+    }
+}
+
+- (void)startLocationManager
+{
+    if ([CLLocationManager locationServicesEnabled]) {
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        [locationManager startUpdatingLocation];
+        updatingLocation = YES;
+    }
+}
+
+- (void)stopLocationManager
+{
+    if (updatingLocation) {
+        [locationManager stopUpdatingLocation];
+        locationManager.delegate = nil;
+        updatingLocation = NO;
+    }
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -32,12 +109,11 @@
     return self;
 }
 
-- (IBAction)getphLocation:(id)sender
-
+- (void)viewDidLoad
 {
-    locationManager.delegate = self;
-    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
-    [locationManager startUpdatingLocation];
+    [super viewDidLoad];
+    [self updateLabels];
+    [self configureGetButton];
 }
 
 #pragma mark - CLLocationManagerDelegate
@@ -45,11 +121,40 @@
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
     NSLog(@"didFailWithError %@", error);
+    
+    if (error.code == kCLErrorLocationUnknown) {
+        return;
+    }
+    
+    [self stopLocationManager];
+    lastLocationError = error;
+    
+    [self updateLabels];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
     NSLog(@"didUpdateToLocation %@", newLocation);
+    
+    if ([newLocation.timestamp timeIntervalSinceNow] < -5.0) {
+        return;
+    }
+    
+    if (newLocation.horizontalAccuracy < 0) {
+        return;
+    }
+    
+    if (location == nil || location.horizontalAccuracy > newLocation.horizontalAccuracy) {
+        
+        lastLocationError = nil;
+        location = newLocation;
+        [self updateLabels];
+        
+        if (newLocation.horizontalAccuracy <= locationManager.desiredAccuracy) {
+            NSLog(@"*** We're done!");
+            [self stopLocationManager];
+        }
+    }
 }
 
 @end
